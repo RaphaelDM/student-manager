05164c0a0242f56016932156a8c548a05ead6867
Raphael DANJARD <r.danjardmaza@lyon.ort.asso.fr>
2026-01-22 15:22:42 +0100

    Fix

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ const { showStudents, addStudent, removeStudent } = require('./src/students');
 showStudents();
 removeStudent('Alice');
 showStudents();
-removeStudent('Alice');
-showStudents();
 addStudent('Marc');
 showStudents();
